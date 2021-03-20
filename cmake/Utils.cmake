aef0bd242bcd86f42dff93ca745fcb99bd2f3186
Benjamin Drost <drost@gfai.de>
2021-03-20 20:00:54 +0100

    CMAKE_SOURCE_DIR => CMAKE_CURRENT_SOURCE_DIR

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -2,7 +2,7 @@
 function (download_model MODEL_NAME MODEL_DOWNLOAD_FLAG MODEL_RELATIVE_PATH CHECKSUM)
   if (MODEL_DOWNLOAD_FLAG)
     message(STATUS "Downloading ${MODEL_NAME} model...")
-    set(MODEL_FILENAME ${CMAKE_SOURCE_DIR}/models/${MODEL_RELATIVE_PATH})
+    set(MODEL_FILENAME ${CMAKE_CURRENT_SOURCE_DIR}/models/${MODEL_RELATIVE_PATH})
     if (NOT EXISTS ${MODEL_FILENAME})
       message(STATUS "NOTE: This process might take several minutes depending on your internet connection.")
       file(DOWNLOAD ${OPENPOSE_URL}${MODEL_RELATIVE_PATH} ${MODEL_FILENAME}
